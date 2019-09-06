aa322643035eac99b02fb4ec2e1b56890a769fec
FritzPunk <54989509+FritzPunk@users.noreply.github.com>
2019-09-06 15:42:03 +0200

    Add files via upload

---
+++ Calculadora De Resistencias Diseñado por Akshat/ResistenceCalculator/ResistorCalculator.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ResistenceCalculator
+{
+    partial class ResistorCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(22, 188);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(410, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(22, 150);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(98, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(124, 150);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(92, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(219, 150);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(108, 21);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(330, 150);
+            this.comboBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(102, 21);
+            this.comboBox4.TabIndex = 7;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(189, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 31);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // ResistorCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 245);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "ResistorCalculator";
+            this.Text = "Diseñado por Akshat";
+            this.Load += new System.EventHandler(this.ResistorCalculator_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ResistorCalculator_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Calculadora De Resistencias Diseñado por Akshat/ResistenceCalculator/ResistorCalculator.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResistenceCalculator
+{
+    public partial class ResistorCalculator : Form
+    {
+        List<String> percentages = new List<String>();
+        int pen_size = 3;
+        List<SolidBrush> pens = new List<SolidBrush>();
+        List<SolidBrush> pens3 = new List<SolidBrush>();
+        List<SolidBrush> pens4 = new List<SolidBrush>();
+        bool draw = false, comb1 = false, comb2 = false, comb3 = false, comb4 = false;
+
+        public ResistorCalculator()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(new string[] { "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey", "White" });
+            comboBox2.Items.AddRange(new string[] { "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey", "White" });
+            comboBox3.Items.AddRange(new string[] { "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Purple" ,"Gold","Silver"});
+            comboBox4.Items.AddRange(new string[] { "Brown", "Red", "Green", "Blue", "Purple","Gray", "Gold", "Silver" });
+            percentages.AddRange(new string[] { "± 1%", "± 2%", "± 0.5%", "± 0.25%", "± 0.10%", "± 0.05%", "± 5%", "± 10%" });
+            pens.AddRange(new SolidBrush[] { new SolidBrush(Color.Black), new SolidBrush(Color.Brown), new SolidBrush(Color.Red), new SolidBrush(Color.Orange), new SolidBrush(Color.Yellow ), new SolidBrush(Color.Green ), new SolidBrush(Color.Blue ), new SolidBrush(Color.Purple ), new SolidBrush(Color.Gray ), new SolidBrush(Color.White ) });
+          //  pens2.AddRange(new SolidBrush[] { new SolidBrush(Color.Black ), new SolidBrush(Color.Brown ), new SolidBrush(Color.Red ), new SolidBrush(Color.Orange ), new SolidBrush(Color.Yellow ), new SolidBrush(Color.Green ), new SolidBrush(Color.Blue ), new SolidBrush(Color.Purple ), new SolidBrush(Color.Gray ), new SolidBrush(Color.White ) });
+            pens3.AddRange(new SolidBrush[] { new SolidBrush(Color.Black ), new SolidBrush(Color.Brown ), new SolidBrush(Color.Red ), new SolidBrush(Color.Orange ), new SolidBrush(Color.Yellow ), new SolidBrush(Color.Green ), new SolidBrush(Color.Blue ), new SolidBrush(Color.Purple ), new SolidBrush(Color.Gold ), new SolidBrush(Color.Silver ) });
+            pens4.AddRange(new SolidBrush[] { new SolidBrush(Color.Brown ), new SolidBrush(Color.Red ), new SolidBrush(Color.Green ), new SolidBrush(Color.Blue ), new SolidBrush(Color.Purple ), new SolidBrush(Color.Gray ), new SolidBrush(Color.Gold ), new SolidBrush(Color.Silver ) });
+           // comboBox1.
+        }
+        
+        
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comb1 = true;
+            Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            draw = false;
+            int a,b,c,d;
+            if(comboBox1.SelectedItem != null && comboBox2.SelectedItem != null  && comboBox3.SelectedItem != null && comboBox4.SelectedItem != null)
+            {
+                draw = true;
+               double result = 0 ;
+                label1.Text =  comboBox1.SelectedIndex.ToString();
+                if(comboBox3.SelectedIndex != 8 && comboBox3.SelectedIndex != 9)
+                result = (comboBox1.SelectedIndex*10 + comboBox2.SelectedIndex ) * Math.Pow(10,comboBox3.SelectedIndex) ;
+                else{
+                    if(comboBox3.SelectedIndex ==8 )
+                    result = (comboBox1.SelectedIndex * 10 + comboBox2.SelectedIndex) *0.1;
+                    else
+                    result = (comboBox1.SelectedIndex * 10 + comboBox2.SelectedIndex) * 0.01;
+                }
+                label1.Text = Convert.ToString(result) + "Ω" + percentages[comboBox4.SelectedIndex];
+            }
+            Refresh();
+
+        }
+        int width =  50;
+        int height = 50;
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comb2 = true;
+            Refresh();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comb3 = true;
+            Refresh();
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comb4 = true;
+            Refresh();
+        }
+
+        private void ResistorCalculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int wire = 10;
+
+
+        int x = 150 ;
+        int y = 35 ;
+
+        //private void draw_resistor(PaintEventArgs e)
+        //{
+            
+        //    // Draw rectangle to screen.
+        //    e.Graphics.FillRectangle(new SolidBrush(Color.Black) , wire1);
+
+        //    e.Graphics.FillRectangle(pens[comboBox1.SelectedIndex], rect);
+        //    e.Graphics.DrawRectangle(new Pen(Color.SandyBrown, pen_size), rect);
+
+        //    e.Graphics.FillRectangle(pens[comboBox2.SelectedIndex], rect2);
+        //    e.Graphics.DrawRectangle(new Pen(Color.SandyBrown, pen_size), rect2);
+
+        //    e.Graphics.FillRectangle(pens3[comboBox3.SelectedIndex], rect3);
+        //    e.Graphics.DrawRectangle(new Pen(Color.SandyBrown, pen_size), rect3);
+
+        //    e.Graphics.FillRectangle(pens4[comboBox4.SelectedIndex], rect4);
+        //    e.Graphics.DrawRectangle(new Pen(Color.SandyBrown, pen_size), rect4);
+
+        //    e.Graphics.FillRectangle(new SolidBrush(Color.Black), wire2);
+        //}
+   
+        private void ResistorCalculator_Paint(object sender, PaintEventArgs e)
+        {
+            // Create rectangle.
+            Rectangle wire1 = new Rectangle(x - width, y + height / 2 - wire / 2, width, wire);
+            Rectangle rect = new Rectangle(x, y, width, height);
+            Rectangle rect2 = new Rectangle(x + width, y, width, height);
+            Rectangle rect3 = new Rectangle(x + width * 2, y, width, height);
+            Rectangle rect4 = new Rectangle(x + width * 3, y, width, height);
+            Rectangle wire2 = new Rectangle(x + width * 4, y + height / 2 - wire / 2, width, wire);
+            // if(draw)
+            //draw_resistor (e);
+            e.Graphics.FillRectangle(new SolidBrush(Color.Black), wire1);
+            e.Graphics.DrawRectangle(new Pen(Color.SandyBrown, pen_size), rect);
+            e.Graphics.DrawRectangle(new Pen(Color.SandyBrown, pen_size), rect2);
+            e.Graphics.DrawRectangle(new Pen(Color.SandyBrown, pen_size), rect3);
+            e.Graphics.DrawRectangle(new Pen(Color.SandyBrown, pen_size), rect4);
+            e.Graphics.FillRectangle(new SolidBrush(Color.Black), wire2);
+            if (comb1)
+                e.Graphics.FillRectangle(pens[comboBox1.SelectedIndex], rect);
+
+            if (comb2)
+                e.Graphics.FillRectangle(pens[comboBox2.SelectedIndex], rect2);
+
+            if (comb3)
+                e.Graphics.FillRectangle(pens3[comboBox3.SelectedIndex], rect3);
+
+            if (comb4)
+                e.Graphics.FillRectangle(pens4[comboBox4.SelectedIndex], rect4);
+            
+        }
+    }
+}
